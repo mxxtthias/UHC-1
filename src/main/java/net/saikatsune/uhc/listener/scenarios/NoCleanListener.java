9624d34b3791970ce23d7281feec400dec31b3a9
maatthi <64573331+mxxtthias@users.noreply.github.com>
2022-02-25 14:24:44 -0500

    Update NoCleanListener.java

--- src/main/java/net/saikatsune/uhc/listener/scenarios/NoCleanListener.java
+++ src/main/java/net/saikatsune/uhc/listener/scenarios/NoCleanListener.java
@@ -32,13 +32,13 @@ public class NoCleanListener implements Listener {
             if(Scenarios.NoClean.isEnabled()) {
                 if(killer != null) {
                     noCleanPlayers.add(killer.getUniqueId());
-                    killer.sendMessage(prefix + ChatColor.GREEN + "[NoClean] You are now protected to any damage for 20 seconds.");
+                    killer.sendMessage(ChatColor.GREEN + "[NoClean] You are now protected to any damage for 20 seconds.");
                     new BukkitRunnable() {
                         @Override
                         public void run() {
                             if(noCleanPlayers.contains(killer.getUniqueId())) {
                                 noCleanPlayers.remove(killer.getUniqueId());
-                                killer.sendMessage(prefix + ChatColor.RED + "[NoClean] You are no longer protected to any damage.");
+                                killer.sendMessage(ChatColor.RED + "[NoClean] You are no longer protected to any damage.");
                             }
                         }
                     }.runTaskLater(game, 20 * 20);
@@ -67,13 +67,13 @@ public class NoCleanListener implements Listener {
 
                 if(noCleanPlayers.contains(damaged.getUniqueId())) {
                     event.setCancelled(true);
-                    damager.sendMessage(prefix + ChatColor.RED + "[NoClean] This player is protected to any damage!");
+                    damager.sendMessage(ChatColor.RED + "[NoClean] This player is protected to any damage.");
                 }
 
                 if(noCleanPlayers.contains(damager.getUniqueId())) {
                     event.setCancelled(true);
                     noCleanPlayers.remove(damager.getUniqueId());
-                    damager.sendMessage(prefix + ChatColor.RED + "[NoClean] You are no longer protected to any damage!");
+                    damager.sendMessage(ChatColor.RED + "[NoClean] You are no longer protected to any damage.");
                 }
             }
         }
